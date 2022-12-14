5766478d7660153509cc3577a5a52f85d95ed690
Her <hgl037310@gmail.com>
2022-12-14 09:53:27 +0800

    s

--- templates/js/product.js
+++ templates/js/product.js
@@ -6,13 +6,21 @@ function ttlCart(){
   var money=0;
   for(var j = 1; j<= 5; j++){
     var scircle= "cartnbs"+j+"Num";
+    var s2circle= "cartnis"+j+"Num";
+    var s3circle= "cartnvs"+j+"Num";
     if(document.getElementById(scircle)){
       money=Number(money)+Number(document.getElementById(scircle).innerHTML)*300;
+    };
+    if(document.getElementById(s2circle)){
+      money=Number(money)+Number(document.getElementById(s2circle).innerHTML)*300;
+    };
+        if(document.getElementById(s3circle)){
+      money=Number(money)+Number(document.getElementById(s3circle).innerHTML)*300;
     };
   };
   document.getElementById("ttlMoneyC").value= money;
   document.getElementById("timeDate").innerHTML= new Date().toLocaleString();
-  document.getElementById("ttlMoney").innerHTML="total:"+ money;
+  document.getElementById("ttlMoney").innerHTML="total: $ "+ money;
   document.getElementById("addCart").innerHTML="";
   closeRightMenu();
   showhistory();
@@ -63,6 +71,50 @@ function addCartNB() {
   }
 }
 
+function addCartNI() {
+  openRightMenu();
+  const x = document.getElementById("addCart");
+  for(var i = 1; i<= 5; i++){
+    var circle= "nis"+i;
+    if (document.getElementById(circle).checked){      
+      var scircle= "cart"+circle;
+      var Dscircle= "'"+scircle+"'";
+      var scircleNum= scircle+"Num"; 
+
+
+      if(!document.getElementById(scircle)){
+        x.innerHTML=x.innerHTML+'<a id="'+scircle+'" class="w3-bar-item w3-button"><img src="img/nike.jpg" style="height:70px"><span>&ensp;NIKE'+ (i+5)+'~'+(i+6)+'&ensp;:&ensp;<span id="'+scircle+'Num">1</span>&ensp;雙</span><button class="w3-bar-item w3-button" onclick="deleteCart('+Dscircle+')">&ensp; &emsp; &emsp; &emsp; - delete &times;</button></a>';
+      }else if(document.getElementById(scircle)&&!document.getElementById(scircleNum)){        
+        document.getElementById(scircle).innerHTML='<img src="img/nike.jpg" style="height:70px"><span>&ensp;NIKE'+ (i+5)+'~'+(i+6)+'&ensp;:&ensp;<span id="'+scircle+'Num">1</span>&ensp;雙</span><button class="w3-bar-item w3-button" onclick="deleteCart('+Dscircle+')">&ensp; &emsp; &emsp; &emsp; - delete &times;</button>';
+      }else{
+        document.getElementById(scircleNum).innerHTML=Number(document.getElementById(scircleNum).innerHTML)+ 1;
+      }      
+    };
+  }
+}
+
+function addCartNV() {
+  openRightMenu();
+  const x = document.getElementById("addCart");
+  for(var i = 1; i<= 5; i++){
+    var circle= "nvs"+i;
+    if (document.getElementById(circle).checked){      
+      var scircle= "cart"+circle;
+      var Dscircle= "'"+scircle+"'";
+      var scircleNum= scircle+"Num"; 
+
+
+      if(!document.getElementById(scircle)){
+        x.innerHTML=x.innerHTML+'<a id="'+scircle+'" class="w3-bar-item w3-button"><img src="img/vans.jpg" style="height:70px"><span>&ensp;VANS'+ (i+5)+'~'+(i+6)+'&ensp;:&ensp;<span id="'+scircle+'Num">1</span>&ensp;雙</span><button class="w3-bar-item w3-button" onclick="deleteCart('+Dscircle+')">&ensp; &emsp; &emsp; &emsp; - delete &times;</button></a>';
+      }else if(document.getElementById(scircle)&&!document.getElementById(scircleNum)){        
+        document.getElementById(scircle).innerHTML='<img src="img/vans.jpg" style="height:70px"><span>&ensp;VANS'+ (i+5)+'~'+(i+6)+'&ensp;:&ensp;<span id="'+scircle+'Num">1</span>&ensp;雙</span><button class="w3-bar-item w3-button" onclick="deleteCart('+Dscircle+')">&ensp; &emsp; &emsp; &emsp; - delete &times;</button>';
+      }else{
+        document.getElementById(scircleNum).innerHTML=Number(document.getElementById(scircleNum).innerHTML)+ 1;
+      }      
+    };
+  }
+}
+
 
 
 function afterLog() {
@@ -103,8 +155,22 @@ function afterLog() {
     x.src = y.src;
   }
 
-
-
+  function hideindex2P(){
+    var x = document.getElementById("insidePage2");
+    x.style.display = "none";
+  }
+  function showindex2P() {
+    var x = document.getElementById("insidePage2");
+    x.style.display = "block";
+  }
+  function hideindex3P(){
+    var x = document.getElementById("insidePage3");
+    x.style.display = "none";
+  }
+  function showindex3P() {
+    var x = document.getElementById("insidePage3");
+    x.style.display = "block";
+  }
   function hidehistory() {
     var x = document.getElementById("history");
     x.style.display = "none";
