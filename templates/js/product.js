57192e9322dbb2d3d56fa83354b3e9111bf2f318
Her <hgl037310@gmail.com>
2022-12-14 09:11:42 +0800

    now

--- templates/js/product.js
+++ templates/js/product.js
@@ -1,42 +1,63 @@
-  window.alert('numa+scircle');
+  // window.alert('numa+scircle');
 
 
 function ttlCart(){
   document.getElementById("addCartCopy").innerHTML= document.getElementById("addCart").innerHTML;
   var money=0;
-  for(var j = 1; j<= 100; j++){
-    var scircle= "cart"+j;
+  for(var j = 1; j<= 5; j++){
+    var scircle= "cartnbs"+j+"Num";
     if(document.getElementById(scircle)){
-      if ((document.getElementById(scircle).style.display!="none")) {
-        money+=300;
-      };
-      }else{
-        break;
+      money=Number(money)+Number(document.getElementById(scircle).innerHTML)*300;
     };
   };
+  document.getElementById("ttlMoneyC").value= money;
   document.getElementById("timeDate").innerHTML= new Date().toLocaleString();
-  document.getElementById("ttlMoney").innerHTML= money;
-  document.getElementById("addCart").style.display="none";
+  document.getElementById("ttlMoney").innerHTML="total:"+ money;
+  document.getElementById("addCart").innerHTML="";
+  closeRightMenu();
+  showhistory();
+  hideindexP();
+  hideproductP();
+  closeInfo();
+
 }
 
 
 var deleteCart = function(numA){
-  var scircle= numA;
-  document.getElementById(scircle).style.display="none";
-  window.alert(numA+scircle);
+  var scircle= numA+"Num";
+  var money = Number(document.getElementById("ttlMoneyC").value);
+  money-=Number(document.getElementById(scircle).innerHTML)*300;
+  document.getElementById("ttlMoneyC").value=money;
+  document.getElementById(numA).innerHTML="";
+  document.getElementById("timeDate").innerHTML= new Date().toLocaleString();
+  document.getElementById("ttlMoney").innerHTML="total:"+ money;
+  
 }
 
 function addCartNB() {
+  openRightMenu();
   const x = document.getElementById("addCart");
   for(var i = 1; i<= 5; i++){
     var circle= "nbs"+i;
     if (document.getElementById(circle).checked){      
       var scircle= "cart"+circle;
+      var Dscircle= "'"+scircle+"'";
+      var scircleNum= scircle+"Num"; 
+      // Create an "li" node:
+      const node = document.createElement("div");
+
+      // Create a text node:
+      const textnode = document.createTextNode("Water");
+
+      // Append the text node to the "li" node:
+      node.appendChild(textnode);
+
       if(!document.getElementById(scircle)){
-        x.innerHTML=x.innerHTML+'<a id="'+scircle+'" class="w3-bar-item w3-button"><img src="img/NB.jpg" style="height:70px">&ensp;NB'+ (i+5)+'~'+(i+6)+'<input type="number" id="'+scircle+'Num" min="1" max="100" value="1"><button class="w3-bar-item w3-button" onclick="deleteCart('+scircle+')">&ensp; &emsp; &emsp; &emsp; - delete &times;</button></a>';
+        x.innerHTML=x.innerHTML+'<a id="'+scircle+'" class="w3-bar-item w3-button"><img src="img/NB.jpg" style="height:70px"><span>&ensp;NB'+ (i+5)+'~'+(i+6)+'&ensp;:&ensp;<span id="'+scircle+'Num">1</span>&ensp;雙</span><button class="w3-bar-item w3-button" onclick="deleteCart('+Dscircle+')">&ensp; &emsp; &emsp; &emsp; - delete &times;</button></a>';
+      }else if(document.getElementById(scircle)&&!document.getElementById(scircleNum)){        
+        document.getElementById(scircle).innerHTML='<img src="img/NB.jpg" style="height:70px"><span>&ensp;NB'+ (i+5)+'~'+(i+6)+'&ensp;:&ensp;<span id="'+scircle+'Num">1</span>&ensp;雙</span><button class="w3-bar-item w3-button" onclick="deleteCart('+Dscircle+')">&ensp; &emsp; &emsp; &emsp; - delete &times;</button>';
       }else{
-        var scircleNum= scircle+"Num";
-        document.getElementById(scircleNum).value=Number(document.getElementById(scircleNum).value)+ 1;
+        document.getElementById(scircleNum).innerHTML=Number(document.getElementById(scircleNum).innerHTML)+ 1;
       }      
     };
   }
@@ -114,5 +135,3 @@ function afterLog() {
   location.replace("product.html")
 }
 
-
-var f
